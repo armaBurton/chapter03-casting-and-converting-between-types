e2412a28c11c4e21e2d230d81a9bb746e944a97e
Arma Burton <arma@armaburton.com>
2023-05-12 13:47:21 -0700

    convert from double to int

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 double b = a; //an int can be safely cast as a double
 WriteLine($"{b}");
 
-// double c = 9.8;
+double c = 9.8;
 // int d = c; //compiler gives error
-// WriteLine($"{d}");
+int d = (int)c; //converts to int losing the decimal and rounding to nearest whole number
+WriteLine($"{d}");
