661cf68d56df5dcd6bfc76398a2f09108a2858f2
Arma Burton <arma@armaburton.com>
2023-05-12 13:51:00 -0700

    from long to int

--- Program.cs
+++ Program.cs
@@ -6,3 +6,11 @@ double c = 9.8;
 // int d = c; //compiler gives error
 int d = (int)c; //converts to int losing the decimal and rounding to nearest whole number
 WriteLine($"{d}");
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+
+e = long.MaxValue;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
