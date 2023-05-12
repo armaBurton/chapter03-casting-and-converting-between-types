8da0e61e5d7cc5bec490220569d9ad169d52d1ce
Arma Burton <arma@armaburton.com>
2023-05-12 13:59:13 -0700

    rounding test

--- Program.cs
+++ Program.cs
@@ -1,23 +1,32 @@
 ﻿using static System.Convert;
 
-int a = 10;
-double b = a; //an int can be safely cast as a double
-WriteLine($"{b}");
+// int a = 10;
+// double b = a; //an int can be safely cast as a double
+// WriteLine($"{b}");
 
-double c = 9.8;
-// int d = c; //compiler gives error
-int d = (int)c; //converts to int losing the decimal and rounding to nearest whole number
-WriteLine($"{d}");
+// double c = 9.8;
+// // int d = c; //compiler gives error
+// int d = (int)c; //converts to int losing the decimal and rounding to nearest whole number
+// WriteLine($"{d}");
 
-long e = 10;
-int f = (int)e;
-WriteLine($"e is {e:N0} and f is {f:N0}");
+// long e = 10;
+// int f = (int)e;
+// WriteLine($"e is {e:N0} and f is {f:N0}");
 
-// e = long.MaxValue; //9,223,372,036,854,775,807
-e = 5_000_000_000;
-f = (int)e;
-WriteLine($"e is {e:N0} and f is {f:N0}");
+// // e = long.MaxValue; //9,223,372,036,854,775,807
+// e = 5_000_000_000;
+// f = (int)e;
+// WriteLine($"e is {e:N0} and f is {f:N0}");
 
-double g = 9.8;
-int h = ToInt32(g);
-WriteLine($"g is {g}, h is {h}");
+// double g = 9.8;
+// int h = ToInt32(g);
+// WriteLine($"g is {g}, h is {h}");
+
+//ROUNDING NUMBERS
+double[] doubles = new[] {
+  9.49, 9.5, 9.51, 10.49, 10.5, 10.51
+};
+
+foreach(double n in doubles){
+  WriteLine($"ToInt32({n}) is {ToInt32(n)}");
+}
